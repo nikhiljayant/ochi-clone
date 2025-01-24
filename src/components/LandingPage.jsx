c26076bb5e9df4bc5466b9665efbf121f616f069
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-01-24 23:32:09 +0530

    landing page started

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="textstructure mt-32">
+        <div className="masker">
+          <h1 className="uppercase text-9xl font-['Founders_Grotesk']">
+            WE CREATE
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
