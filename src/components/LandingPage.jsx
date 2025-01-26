3eb680e3dc94b02bdde3cf6d8ccb40d56b390166
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-01-26 20:09:40 +0530

    font correction and landing page improvisation

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,13 +3,14 @@ import React from "react";
 const LandingPage = () => {
   return (
     <div className="w-full h-screen bg-zinc-900 pt-1">
-      <div className="textstructure mt-32">
-        <div className="masker">
-          <h1 className="uppercase text-9xl font-['Founders_Grotesk']">
-            WE CREATE
-          </h1>
-          {/* 31:25 */}
-        </div>
+      <div className="textstructure mt-52 px-20">
+        {["We Create", "Eye Opening", "Presentations"].map((txt) => (
+          <div key={txt} className="masker">
+            <h1 className="uppercase text-[7.5vw] leading-[6vw] tracking-tighter font-['Host_Grotesk'] font-medium">
+              {txt}
+            </h1>
+          </div>
+        ))}
       </div>
     </div>
   );
