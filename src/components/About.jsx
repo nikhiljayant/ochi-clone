dbdc72fbab896447c3c3c22ede8f2557e5ac3b68
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-01-29 23:53:58 +0530

    about us started

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <div>{/* 1:09:00 */}</div>;
+  return (
+    <div className="w-full p-20 bg-[#CDEA68] rounded-t-3xl text-black">
+      <h1 className="font-['Neue_Montreal] text-[3.5vw] leading-[4.5vw] tracking-tight">
+        Ochi is a strategic presentation agency for forward-thinking businesses
+        that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and
+        hire great peo­ple.
+      </h1>
+    </div>
+  );
 };
 
 export default About;
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,10 +4,10 @@ const Marquee = () => {
   return (
     <div className="w-full py-20 rounded-t-3xl bg-[#004D43]">
       <div className="text border-t border-slate-300 flex gap-10 overflow-hidden">
-        <h1 className="text-[27vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] whitespace-nowrap uppercase -mt-20">
+        <h1 className="text-[27vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] whitespace-nowrap uppercase -mt-[7vw]">
           we are ochi
         </h1>
-        <h1 className="text-[27vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] whitespace-nowrap uppercase -mt-20">
+        <h1 className="text-[27vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] whitespace-nowrap uppercase -mt-[7vw]">
           we are ochi
         </h1>
       </div>
