9929e760218ed91f4368bb1dbda5abc8a581050f
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-01-31 00:00:23 +0530

    about and marquee section changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,18 @@ const About = () => {
         that need to raise funds, sell prod­ucts, ex­plain com­plex ideas, and
         hire great peo­ple.
       </h1>
+      <div className="w-full flex border-t-[1px] pt-10 mt-20 border-[#a1b562]">
+        <div className="w-1/2">
+          <h1 className="font-['Neue_Montreal] text-5xl">Our approach:</h1>
+          <button className="flex gap-10 items-center py-3 px-5 bg-zinc-900 mt-7 rounded-full text-zinc-100 text-lg">
+            READ MORE
+            <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
+          </button>
+        </div>
+        <div className="w-1/2 h-[70vh]">
+          <img className="w-full h-full rounded-3xl object-cover" src="https://ochi.design/wp-content/uploads/2022/05/Homepage-Photo-663x469.jpg" alt="img" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Marquee = () => {
   return (
     <div className="w-full py-20 rounded-t-3xl bg-[#004D43]">
-      <div className="text border-t border-slate-300 flex gap-10 overflow-hidden">
-        <h1 className="text-[27vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] whitespace-nowrap uppercase -mt-[7vw]">
+      <div className="text border-t border-b border-slate-300 flex gap-10 overflow-hidden pt-10">
+        <h1 className="text-[27vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] whitespace-nowrap uppercase -mt-[7vw] -mb-[1vw]">
           we are ochi
         </h1>
-        <h1 className="text-[27vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] whitespace-nowrap uppercase -mt-[7vw]">
+        <h1 className="text-[27vw] leading-none font-['Test_Founders_Grotesk_X_Condensed'] whitespace-nowrap uppercase -mt-[7vw] -mb-[1vw]">
           we are ochi
         </h1>
       </div>
