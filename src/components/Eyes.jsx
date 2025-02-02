da7b923e2bf5432872659626624c91fafe4bd4ce
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-02-02 23:50:48 +0530

    changes

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Eyes = () => {
   return (
-    <div>Eyes</div>
-  )
-}
+    <div className="eyes w-full h-screen overflow-hidden">
+      <div className="w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]"></div>
+    </div>
+  );
+};
 
-export default Eyes
+export default Eyes;
