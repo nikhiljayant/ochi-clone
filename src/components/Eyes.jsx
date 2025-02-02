7bd26911dec2cd0b83407d59ef5c40e6bd78e6e5
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-02-03 00:26:14 +0530

    eye structure completed

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -3,7 +3,26 @@ import React from "react";
 const Eyes = () => {
   return (
     <div className="eyes w-full h-screen overflow-hidden">
-      <div className="w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]"></div>
+      <div className="relative w-full h-full bg-cover bg-center bg-[url('https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-1440x921.jpg')]">
+        <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
+          {/* Eye 1 */}
+          <div className="flex justify-center items-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
+            <div className="relative w-2/3 h-2/3 flex items-center justify-center rounded-full bg-zinc-900">
+              <div className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10">
+                <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+          {/* Eye 2 */}
+          <div className="flex justify-center items-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
+            <div className="relative w-2/3 h-2/3 flex items-center justify-center rounded-full bg-zinc-900">
+              <div className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10">
+                <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
