9c7ace5ed9fadfb6135a1554ecbb564d58c1cc0e
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-02-03 23:48:00 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // Components
 import About from "./components/About";
 import Eyes from "./components/Eyes";
+import Featured from "./components/Featured";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
 import Navbar from "./components/Navbar";
@@ -13,6 +14,7 @@ function App() {
       <Marquee />
       <About />
       <Eyes />
+      <Featured />
     </div>
   );
 }
