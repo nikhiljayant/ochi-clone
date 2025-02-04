fc981b212a9adb036031f341b12e80e208486c2b
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-02-04 23:39:23 +0530

    changes

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <div className="w-full py-20">
+      <div className="w-full px-20 border-b-[1px] border-zinc-700 pb-20">
+        <h1 className="text-7xl font-['Neue_Montreal'] tracking-tight">
+          Featured projects
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default Featured
+export default Featured;
