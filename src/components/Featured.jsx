91f878fcf9c385e0ba2fe2835b5ff7fbbfc54722
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-02-06 23:48:07 +0530

    minor change

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -10,8 +10,16 @@ const Featured = () => {
       </div>
       <div className="px-20">
         <div className="cards w-full flex gap-10 mt-10">
-          <div className="card w-1/2 h-[80vh] bg-red-500"></div>
-          <div className="card w-1/2 h-[80vh] bg-red-500"></div>
+          <div className="cardcontainer w-1/2 h-[75vh]">
+            <div className="card w-full h-full rounded-xl overflow-hidden">
+              <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-1326x1101.png" alt="" />
+            </div>
+          </div>
+          <div className="cardcontainer w-1/2 h-[75vh]">
+            <div className="card w-full h-full rounded-xl overflow-hidden">
+              <img className="w-full h-full bg-cover" src="https://ochi.design/wp-content/uploads/2024/08/Frame-481692-1-1326x1101.png" alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
