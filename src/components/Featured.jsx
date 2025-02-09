4d18492dc9a7a9d25ff9d78e3827b045897adc44
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2025-02-10 00:27:19 +0530

    completed

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+// Framer Motion
+import { motion } from "framer-motion";
+import { Power4 } from "gsap";
 
 const Featured = () => {
+  const [currentlyHovered, setCurrentlyHovered] = useState(null);
+
   return (
-    <div className="w-full py-20">
+    <div data-scroll data-scroll-section className="relative w-full py-20">
       <div className="w-full px-20 border-b-[1px] border-zinc-700 pb-20">
         <h1 className="text-7xl font-['Neue_Montreal'] tracking-tight">
           Featured projects
@@ -10,9 +15,22 @@ const Featured = () => {
       </div>
       <div className="px-20">
         <div className="cards w-full flex gap-10 mt-10">
-          <div className="cardcontainer relative w-1/2 h-[75vh]">
-            <h1 className="text-[#CDEA68] absolute left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-8xl">
-              FYDE
+          <div
+            onMouseEnter={() => setCurrentlyHovered("fyde")}
+            onMouseLeave={() => setCurrentlyHovered(null)}
+            className="cardcontainer relative w-1/2 h-[75vh]"
+          >
+            <h1 className="text-[#CDEA68] absolute flex overflow-hidden left-full -translate-x-1/2 top-1/2 -translate-y-1/2 text-8xl z-10">
+              <motion.span
+                initial={{ y: "100%" }}
+                animate={
+                  currentlyHovered === "fyde" ? { y: "0" } : { y: "100%" }
+                }
+                transition={{ ease: [0.22, 1, 0.36, 1], delay: 0.3 }}
+                className="inline-block"
+              >
+                FYDE
+              </motion.span>
             </h1>
             <div className="card w-full h-full rounded-xl overflow-hidden">
               <img
@@ -22,9 +40,22 @@ const Featured = () => {
               />
             </div>
           </div>
-          <div className="cardcontainer relative w-1/2 h-[75vh]">
-            <h1 className="text-[#CDEA68] absolute right-full translate-x-1/2 top-1/2 -translate-y-1/2 text-8xl">
-              VISE
+          <div
+            onMouseEnter={() => setCurrentlyHovered("vise")}
+            onMouseLeave={() => setCurrentlyHovered(null)}
+            className="cardcontainer relative w-1/2 h-[75vh]"
+          >
+            <h1 className="text-[#CDEA68] flex overflow-hidden absolute right-full translate-x-1/2 top-1/2 -translate-y-1/2 text-8xl">
+              <motion.span
+                initial={{ y: "100%" }}
+                animate={
+                  currentlyHovered === "vise" ? { y: "0" } : { y: "100%" }
+                }
+                transition={{ ease: [0.22, 1, 0.36, 1], delay: 0.3 }}
+                className="inline-block"
+              >
+                VISE
+              </motion.span>
             </h1>
             <div className="card w-full h-full rounded-xl overflow-hidden">
               <img
